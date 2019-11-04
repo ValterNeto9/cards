f1f38e4ce32593f22bc6ed9c6622b598bafb984c
Valter Ferreira Lopes Neto <valter.neto@gft.com>
2019-11-04 18:15:27 -0300

    alter create_deck function and create deal and save functions

--- cards.ex
+++ cards.ex
@@ -1,24 +1,40 @@
 defmodule Cards do
 
-  def create_deck do
-    ["Ace", "two", "three"]
-  end
+    def create_deck do
+        suits = ["spades","hearts","diamonds","clubs"]
+        values = ["ace", "one", "two", "three", "four", "five", "six", "seven", "nine", "ten", "jack", "queen", "king"]
 
-  def shuffle( deck ) do
-    Enum.shuffle( deck )
-  end
+        for suit <- suits, value <- values do
+          "#{ value } of #{ suit }"
+        end
 
-  def contains?( deck, card ) do
-    Enum.member?( deck, card)
-  end
+    end
 
+    def shuffle( deck ) do
+        Enum.shuffle( deck )
+    end
 
-  def exec( args ) do
-    deck = Cards.create_deck
-    #shuffle( deck )
-    contains?( deck, args )
-  end
+    def contains?( deck, card ) do
+        Enum.member?( deck, card)
+    end
 
+    def deal( deck, hand_size ) do
+      Enum.split( deck, hand_size )
+    end 
+
+    def save( deck, filename ) do 
+      binary = :erlang.term_to_binary( deck )
+      File.write( filename, binary )
+    end
+
+    def exec( args ) do
+        deck = Cards.create_deck
+        #shuffle( deck )
+        #contains?( deck, args )
+        #deal( deck, args)
+        #save( deck, args )
+    end
+    
 end
 
-IO.inspect Cards.exec( "two" )
+IO.inspect Cards.exec( "arquivo_teste" )
